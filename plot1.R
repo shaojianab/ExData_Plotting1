b07aeaaf96b51a43113ef1cda33f2ebf2f6a92f5
shaojianab <shaojianab@qq.com>
2015-01-08 17:21:55 +0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -4,6 +4,3 @@ b<-a[which(a$Date=="1/2/2007" | a$Date=="2/2/2007"),]
 png(file="plot1.png")
 hist(b,main="Global Active Power",xlab="Global Active Power(kilowatts)",col="red")
 dev.off() 
-
-
-![plot1](H:/Coursera/Exploratory Data Analysis/plot1.png) 
