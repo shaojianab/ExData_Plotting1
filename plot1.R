1169f60c0e1d9a128f0fd9ea1a65dcf3b7c3d5c6
shaojianab <shaojianab@qq.com>
2015-01-08 16:56:08 +0800

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,6 @@
+setwd("H:\\Coursera\\Exploratory Data Analysis")
+a<-read.table("household_power_consumption.txt",sep=";",header=TRUE)
+b<-a[which(a$Date=="1/2/2007" | a$Date=="2/2/2007"),]
+png(file="plot1.png")
+hist(b,main="Global Active Power",xlab="Global Active Power(kilowatts)",col="red")
+dev.off() 
